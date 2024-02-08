b1d9e6f066544037ac20546410ac4a7b705c9c48
Antonio <physrumoa@gmail.com>
2024-02-08 17:05:17 +0000

    substitute by zero array wiht cond

--- dynamic_slice.py
+++ dynamic_slice.py
@@ -1,7 +1,17 @@
 @jit
-def f_eduardo(identity_for_n: int, array_of_H):
+def f_eduardo(identity_for_n, array_of_H):
     n = identity_for_n.shape[0]
     return jax.lax.dynamic_slice(array_of_H, (0, 0, 0), (n, 2, 2))
 
 
 f_eduardo(jnp.ones(6), array_of_h)
+
+
+jax.vmap((
+    lambda i, arr: jax.lax.cond(
+         i < 1.5,
+        lambda i, arr: arr,
+        lambda i, arr: 0 * arr,
+        *[i, arr]
+    )
+), in_axes=(0, 0))(times, A)
